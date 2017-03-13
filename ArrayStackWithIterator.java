6ac432b52e3313a66d6e333dccb2f5963deb6d0d
Liang Dong <liangdong@cpp.edu>
2017-03-12 17:04:29 -0700

    CS240_FINAL V1.1

--- ArrayStackWithIterator.java
+++ ArrayStackWithIterator.java
@@ -1,5 +1,6 @@
 package cs240_final;
 
+import java.util.EmptyStackException;
 import java.util.NoSuchElementException;
 import homework2.ArrayStack;
 
@@ -10,6 +11,25 @@ import homework2.ArrayStack;
  *
  */
 public class ArrayStackWithIterator<T> extends ArrayStack<T> {
+	
+	private final T[] array;
+	private int indexOfTopElement;
+	private static final int DEFAULT_CAPACITY = 20;
+	private boolean initialized = false;
+
+	/** Creates an empty bag whose initial capacity is 10. */
+	public ArrayStackWithIterator(int capacity) {
+		@SuppressWarnings("unchecked")
+		T[] tempArr = (T[]) new Object[capacity];
+		array = tempArr;
+		indexOfTopElement = -1;
+		initialized = true;
+	}//end constructor
+	
+	/** Creates an empty bag whose initial capacity is 10. */
+	public ArrayStackWithIterator() {
+		this(DEFAULT_CAPACITY);
+	}//end constructor
 	
 	/**
 	 * @return An iterator.
@@ -27,11 +47,9 @@ public class ArrayStackWithIterator<T> extends ArrayStack<T> {
 		private int previousPosition;
 		@SuppressWarnings("unused")
 		private boolean wasNextCalled;
-		private T[] arr;
 		
 		private IteratorForArrayStack() {
-			arr = toArray();
-			nextPosition = getIndexOfTopElement();
+			nextPosition = indexOfTopElement;
 			previousPosition = nextPosition + 1;
 			wasNextCalled = false;
 		}
@@ -52,7 +70,7 @@ public class ArrayStackWithIterator<T> extends ArrayStack<T> {
 		 */
 		public T next() {
 			if(hasNext()) {
-				T temp = arr[nextPosition];
+				T temp = array[nextPosition];
 				nextPosition--;
 				previousPosition--;
 				wasNextCalled = true;
@@ -78,7 +96,7 @@ public class ArrayStackWithIterator<T> extends ArrayStack<T> {
 		 * @throws NoSuchElementException if the iterator is before the fisrt entry.
 		 */
 		public boolean hasPrevious() {
-			return previousPosition <= getIndexOfTopElement();
+			return previousPosition <= indexOfTopElement;
 		}
 		
 		/**
@@ -88,7 +106,7 @@ public class ArrayStackWithIterator<T> extends ArrayStack<T> {
 		 */
 		public T previous() {
 			if(hasPrevious()) {
-				T temp = arr[previousPosition];
+				T temp = array[previousPosition];
 				previousPosition++;
 				nextPosition++;
 				wasNextCalled = true;
@@ -99,5 +117,88 @@ public class ArrayStackWithIterator<T> extends ArrayStack<T> {
 			}
 		}
 	}
+	
 
+
+	/** Adds a new entry to this bag.
+    @param newEntry The object to be added as a new entry.
+	*/
+	@Override
+	public void push(T newEntry) {
+		checkInitialization();
+		if(!isFull()) {
+			indexOfTopElement++;
+			array[indexOfTopElement] = newEntry;
+		}//end if
+	}//end push
+
+	/** Removes and returns this stack's top entry.
+     * @return  The object at the top of the stack. 
+	 * @throws EmptyStackException 
+	 */
+	@Override
+	public T pop(){
+		checkInitialization();
+		if(isEmpty()) {
+			throw new EmptyStackException();
+		}
+		else {
+			T temp = array[indexOfTopElement];
+			array[indexOfTopElement] = null;
+			indexOfTopElement--;
+			return temp;
+		}
+	}
+
+	/** Retrieves this stack's top entry.
+    * @return  The object at the top of the stack.
+    * @throws  EmptyStackException if the stack is empty. 
+    */
+	@Override
+	public T peek() {
+		checkInitialization();
+		if(isEmpty()) {
+			throw new EmptyStackException();
+		}
+		else {
+			return array[indexOfTopElement];
+		}
+	}
+
+	/** Detects whether this stack is empty.
+    * @return  True if the stack is empty. 
+    */
+	@Override
+	public boolean isEmpty() {
+		return indexOfTopElement < 0 ;
+	}
+
+	/** Removes all entries from this stack. 
+	*/
+	@Override
+	public void clear() {
+		while(!isEmpty()) {
+			array[indexOfTopElement] = null;
+			indexOfTopElement--;
+		}
+	}
+	
+	/** Throws an exception if this object is not initialized.
+     * 
+     */
+    private void checkInitialization()
+    {
+        if (!initialized)
+             throw new SecurityException("ArrayStack object is not initialized " +
+                                        "properly.");
+   }
+	
+	
+	/** Detects whether this stack is full.
+    * @return  True if the stack is full. 
+    */
+	public boolean isFull() {
+		return indexOfTopElement >= array.length - 1;
+	}// end isFull
+	
 }
